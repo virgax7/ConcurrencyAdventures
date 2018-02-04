c211b888f97e6772faf965c2b6c748604dfd2a36
pyoung <pyoungx7@gmail.com>
2018-02-04 17:21:37 -0500

    DelegatingThreadSafetyExample

--- src/pattern/delegating_thread_safety/FittingRoomDelegatingThreadSafetyExample.java
+++ src/pattern/delegating_thread_safety/FittingRoomDelegatingThreadSafetyExample.java
@@ -1,8 +1,10 @@
-package pattern.monitor;
+package pattern.delegating_thread_safety;
+
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import static java.util.stream.IntStream.range;
 
-public class MonitorExample {
+public class FittingRoomDelegatingThreadSafetyExample {
     public static void main(String[] args) {
         final FittingRoom fittingRoom = new FittingRoom();
         final String[] names = {"paul", "helen", "huong", "lana", "bethany", "nathali", "julianna"};
@@ -18,22 +20,22 @@ public class MonitorExample {
     }
 }
 
-// all its state, which is just isVacant, is guarded by it's own intrinsic lock
+// all its state, isVacant, is thread-safe
 class FittingRoom {
-    boolean isVacant = true;
+    final AtomicBoolean isVacant = new AtomicBoolean(true);
 
     synchronized void enter(final String name) throws InterruptedException {
-        while (!isVacant) {
+        while (!isVacant.get()) {
             wait();
         }
         System.out.println(name + " has entered the fitting room");
-        isVacant = false;
+        isVacant.set(false);
         tryOnClothes();
     }
 
     synchronized void leave(final String name) {
         System.out.println(name + " has left the fitting room");
-        isVacant = true;
+        isVacant.set(true);
         notify();
     }
 
---
+++ src/pattern/monitor/FittingRoomMonitorExample.java
@@ -0,0 +1,43 @@
+package pattern.monitor;
+
+import static java.util.stream.IntStream.range;
+
+public class FittingRoomMonitorExample {
+    public static void main(String[] args) {
+        final FittingRoom fittingRoom = new FittingRoom();
+        final String[] names = {"paul", "helen", "huong", "lana", "bethany", "nathali", "julianna"};
+        range(0, 7).forEach(i -> {
+            new Thread(() -> {
+                try {
+                    fittingRoom.enter(names[i]);
+                    fittingRoom.leave(names[i]);
+                } catch (InterruptedException consume) {
+                }
+            }).start();
+        });
+    }
+}
+
+// all its state, which is just isVacant, is guarded by it's own intrinsic lock
+class FittingRoom {
+    boolean isVacant = true;
+
+    synchronized void enter(final String name) throws InterruptedException {
+        while (!isVacant) {
+            wait();
+        }
+        System.out.println(name + " has entered the fitting room");
+        isVacant = false;
+        tryOnClothes();
+    }
+
+    synchronized void leave(final String name) {
+        System.out.println(name + " has left the fitting room");
+        isVacant = true;
+        notify();
+    }
+
+    void tryOnClothes() throws InterruptedException {
+        Thread.sleep(200);
+    }
+}
