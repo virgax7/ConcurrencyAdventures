859b3ce496fee28ade8ad1a7d33af7d8e289d1a0
pyoung <pyoungx7@gmail.com>
2018-02-04 22:34:15 -0500

    ThreadinterruptBehaviorTest

---
+++ src/random/ThreadInterruptBehaviorTest.java
@@ -0,0 +1,31 @@
+package random;
+
+public class ThreadInterruptBehaviorTest {
+    public static void main(String[] args) throws InterruptedException {
+        final Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 100000000000000000L; i++) {
+            }
+            System.out.println("Would this print immediately after interruption?");
+            System.out.println("Nope, interrupt merely sets a flag indicating whether its interrupted");
+            System.out.println("It's up to you how you deal with that interruption...");
+        });
+        // uncomment to see this in action
+//        t1.start();
+        t1.interrupt();
+        System.out.println("t1.join called");
+
+        // Here is one that will actually stop the loop in the thread by using a polling system
+        final Thread t2 = new Thread(() -> {
+            for (int i = 0; i < 100000000000000000L; i++) {
+                if (Thread.currentThread().isInterrupted()) {
+                    break;
+                }
+            }
+            // calling isInterrupted clears the interruption status so we reset it
+            Thread.currentThread().interrupt();
+        });
+        t2.start();
+        t2.interrupt();
+        System.out.println(t2.isInterrupted());
+    }
+}
